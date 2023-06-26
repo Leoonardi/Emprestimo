5b7b83df4085bb2238f4c2389f321d0652118141
matheus.fleonardi <matheus.fleonardi@SBC0601501W10-1>
2023-06-26 08:22:44 -0300

    Adicionar arquivos de projeto.

---
+++ Emprestimo/Emprestimo.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Emprestimo
+{
+    partial class Emprestimo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
+            this.txtcpfCliente = new System.Windows.Forms.TextBox();
+            this.txtValorLivro = new System.Windows.Forms.TextBox();
+            this.txtNomeLivro = new System.Windows.Forms.ComboBox();
+            this.txtDescontoLivro = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(580, 318);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(156, 91);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(312, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Emprestimo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(60, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(60, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cpf Cliente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(60, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nome Livro";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(60, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Valor Livro";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(60, 351);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 21);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Desconto Livro";
+            // 
+            // txtNomeCliente
+            // 
+            this.txtNomeCliente.Location = new System.Drawing.Point(186, 107);
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(100, 23);
+            this.txtNomeCliente.TabIndex = 8;
+            // 
+            // txtcpfCliente
+            // 
+            this.txtcpfCliente.Location = new System.Drawing.Point(186, 166);
+            this.txtcpfCliente.Name = "txtcpfCliente";
+            this.txtcpfCliente.Size = new System.Drawing.Size(100, 23);
+            this.txtcpfCliente.TabIndex = 9;
+            // 
+            // txtValorLivro
+            // 
+            this.txtValorLivro.Location = new System.Drawing.Point(186, 284);
+            this.txtValorLivro.Name = "txtValorLivro";
+            this.txtValorLivro.Size = new System.Drawing.Size(100, 23);
+            this.txtValorLivro.TabIndex = 10;
+            // 
+            // txtNomeLivro
+            // 
+            this.txtNomeLivro.FormattingEnabled = true;
+            this.txtNomeLivro.Location = new System.Drawing.Point(186, 228);
+            this.txtNomeLivro.Name = "txtNomeLivro";
+            this.txtNomeLivro.Size = new System.Drawing.Size(121, 23);
+            this.txtNomeLivro.TabIndex = 11;
+            // 
+            // txtDescontoLivro
+            // 
+            this.txtDescontoLivro.FormattingEnabled = true;
+            this.txtDescontoLivro.Location = new System.Drawing.Point(186, 349);
+            this.txtDescontoLivro.Name = "txtDescontoLivro";
+            this.txtDescontoLivro.Size = new System.Drawing.Size(121, 23);
+            this.txtDescontoLivro.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(562, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // Emprestimo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDescontoLivro);
+            this.Controls.Add(this.txtNomeLivro);
+            this.Controls.Add(this.txtValorLivro);
+            this.Controls.Add(this.txtcpfCliente);
+            this.Controls.Add(this.txtNomeCliente);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Emprestimo";
+            this.Text = "Emprestimo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btncalcular;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtNomeCliente;
+        private TextBox txtcpfCliente;
+        private TextBox txtValorLivro;
+        private ComboBox txtNomeLivro;
+        private ComboBox txtDescontoLivro;
+        private GroupBox groupBox1;
+    }
+}
